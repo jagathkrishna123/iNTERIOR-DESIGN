0bdcd0c040adaaf302ae6a5410f0b300a1822b6c
jagathkrishna123 <jagathkrishna900@gmail.com>
2026-05-30 20:13:30 +0530

    12th commit

--- src/Pages/Landingpage.jsx
+++ src/Pages/Landingpage.jsx
@@ -24,6 +24,7 @@ import {  IoIosCloseCircleOutline } from "react-icons/io";
 const Landingpage = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [openForm, setOpenForm] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
 
 
   return (
@@ -413,6 +414,7 @@ const Landingpage = () => {
 
           {/* Button (Desktop only) */}
           <button
+            onClick={() => setOpenModal(true)}
             className="
             hidden
             md:flex
@@ -691,13 +693,96 @@ const Landingpage = () => {
           <p className="text-white text-[13px] leading-[1.5] font-light max-w-[255px] font-pop">
             Refined spaces shaped by Italian aesthetics, natural materials, and modern living.
           </p>
-          <button className="w-full bg-white text-black py-4 px-6 text-[13px] rounded-sm font-medium flex items-center justify-between hover:bg-[#f2f2f2] transition-all duration-300">
+          <button 
+          onClick={() => setOpenModal(true)}
+          className="w-full bg-white text-black py-4 px-6 text-[13px] rounded-sm font-medium flex items-center justify-between hover:bg-[#f2f2f2] transition-all duration-300">
             Book a Consultation
             <span className="text-lg">→</span>
           </button>
         </div>
 
       </section>
+
+       <AnimatePresence>
+
+        {openModal && (
+
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="fixed inset-0 z-50 bg-black/60 backdrop-blur-sm flex items-center justify-center px-4"
+          >
+
+            {/* Modal */}
+            <motion.div
+              initial={{ y: 80, opacity: 0, scale: 0.95 }}
+              animate={{ y: 0, opacity: 1, scale: 1 }}
+              exit={{ y: 40, opacity: 0, scale: 0.95 }}
+              transition={{
+                duration: 0.45,
+                ease: [0.22, 1, 0.36, 1],
+              }}
+              className="w-full max-w-[520px] font-outfit bg-[#1F2123] border border-white/10 p-7 sm:p-10 relative rounded-xs"
+            >
+
+              {/* Close Button */}
+              <button
+                onClick={() => setOpenModal(false)}
+                className="absolute top-5 right-5 text-white/60 hover:text-white text-lg md:text-xl transition"
+              >
+                <IoIosCloseCircleOutline/>
+
+              </button>
+
+              <h2 className="text-[30px] sm:text-[40px] font-light text-gray-400 tracking-[-0.03em] mb-2">
+                Book a Consultation
+              </h2>
+
+              <p className="text-white/50 text-sm mb-8">
+                Tell us about your dream space.
+              </p>
+
+              {/* Form */}
+              <form className="space-y-5">
+
+                <input
+                  type="text"
+                  placeholder="Full Name"
+                  className="w-full h-[54px] text-gray-500 bg-transparent border border-white/10 px-4 text-sm outline-none focus:border-white/40 transition"
+                />
+
+                <input
+                  type="email"
+                  placeholder="Email Address"
+                  className="w-full h-[54px] text-gray-500 bg-transparent border border-white/10 px-4 text-sm outline-none focus:border-white/40 transition"
+                />
+
+                <input
+                  type="text"
+                  placeholder="Project Type"
+                  className="w-full h-[54px] text-gray-500 bg-transparent border border-white/10 px-4 text-sm outline-none focus:border-white/40 transition"
+                />
+
+                <textarea
+                  rows="5"
+                  placeholder="Tell us more about your project..."
+                  className="w-full text-gray-500 bg-transparent border border-white/10 p-4 text-sm outline-none resize-none focus:border-white/40 transition"
+                />
+
+                <motion.button
+                  whileHover={{ scale: 1.02 }}
+                  whileTap={{ scale: 0.98 }}
+                  className="w-full h-[56px] bg-slate-300 text-black text-sm tracking-wide hover:bg-[#EAEAEA] transition rounded-xs"
+                >
+                  Submit Request
+                </motion.button>
+
+              </form>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
 
       {/* grey section ....................................................*/}
 
